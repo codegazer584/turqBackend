423350cabacc66b80a061c8333b37ad5453c57ea
Michael Conlon <michael@conlon.io>
2021-06-13 14:40:17 -0400

    Updating test data to reproduce bug When payments are added to an issue the issue will no longer be editable

--- db/create/3_db_test_data.sql
+++ db/create/3_db_test_data.sql
@@ -1,3 +1,8 @@
 INSERT INTO users (username, first_name, last_name, email, password) VALUES ('mbconlon', 'Michael', 'Conlon', 'michael@conlon.io', 'testPass');
+INSERT INTO users (username, first_name, last_name, email, password) VALUES ('admin', 'admin', 'turq', 'admin@turq.io', '$2a$10$Xl1EV9MgK.2Ufk/OtCixFebqVElptJaC1VUBAcpypSFQGdyr7AkvS');
 
 INSERT INTO contests (id, title, end_date, rules, criteria, description, approved, author_id, status_id) values (1, 'test contest', '2021-05-11 19:50:11.000000', 'dd', '', '', 'true', 1, 0);
+INSERT INTO contests (id, title, end_date, rules, criteria, description, approved, author_id, status_id) values (2, 'test contest', '2021-05-11 19:50:11.000000', 'dd', '', '', 'true', 2, 0);
+
+INSERT INTO payments (id, amount, author_id, contest_id, status) values (1, 10000, 1, 2, 0);
+INSERT INTO payments (id, amount, author_id, contest_id, status) values (2, 40000, 2, 2, 0);
